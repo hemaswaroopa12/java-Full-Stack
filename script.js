fdc41a43c9ec9c0cbe54fd4a06e863eb82db1e7b
hemaswaroopa12 <hemaswaroopa329@gmail.com>
2026-06-11 16:03:53 +0530

    js

---
+++ script.js
@@ -0,0 +1,123 @@
+/*let name="roopa";
+var age=19;
+const height="1.72";
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(age+5);
+console.log (age-3);
+console.log(age*2);
+console.log(age/2);
+console.log(age%2);
+age+=5;
+console.log(age);
+age-=2;
+console.log(age);
+age*=2;
+console.log(age);
+age/=2;
+console.log(age);
+age%=2;
+console.log(age);
+let age1=19;
+console.log(age>18);
+console.log(age<18); 
+console.log(age>=18);
+console.log(age<=18);
+console.log(age==18);
+console.log(age!=18);
+let food="pizza";
+let breakfast="pancakes";
+let food2="dosa";
+let food3="idli";
+let food4="vada";
+let food5="upma";
+ if(food==breakfast){
+    console.log("its a match");
+ }
+ else{
+    console.log("not a match");
+ }
+    task 
+    let num=3600;
+    if(num>500)
+    { 
+        num=num%500;
+
+        console.log("500 "+Math.floor(3600/500));
+    }
+    if(num<=200)
+    {
+        console.log("200 "+Math.floor(num/200));
+            num%=200;
+        console.log(num);
+    }
+    if(num>=100)
+    {      
+       console.log("100 "+Math.floor(num/100));
+        num%=100;
+    }
+   for(let i=1;i<=30;i++){
+    console.log("day " +i+ " completed");
+   }
+  let i=1;
+  while(i<=30){
+    console.log("day " +i+ " completed");
+  }
+ let user_name="roopa";
+    let password="roopa123";
+    let name="abhi";
+   let  pin="nanna123";
+   let name1="kavya";
+   let pin1="kavya123";
+   let name2="navya";
+   let pin2="navya123";
+
+   if(name=="abhi" ){
+    console.log(user_name);
+    if(pin=="nanna123"){
+        console.log("loged in sucessfully as abhi");
+    }
+}
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=10;j++){
+        console.log(i+"*"+j+"="+i*j);
+}
+}
+let num=10;
+for(let i=1;i<=10;i++){
+    if(i%2!=0){
+        console.log(i);
+        
+
+    }
+    
+    
+}
+for(let i=1;i<=10;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+console.log("last gift received " +num);  
+   
+  function cook(){
+    console.log("pour the flour in tawa");
+    console.log("rub it  round order with gareta");
+    console.log("spill some uil above it then wait ");
+    console.log("twist it with dosa gareta");
+    console.log("atlast gently serve it to navya")
+  }      
+  for( let i=1;1<=2;i++){
+    cook();
+  }*/
+   let chocolates=["dark chocolate","dairy milk","perk","much","kachaa","kitkat"];
+   let sales={
+    abhi:chocolates[0],
+    vasu:chocolates[1],
+    sai:chocolates[2],
+    raju:chocolates[3],
+    roopa:chocolates[4],
+    kushi:chocolates[5],
+
+   }
