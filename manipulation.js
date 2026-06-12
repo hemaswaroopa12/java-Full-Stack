46af50f02261edeaf6f89d56d3b2a1d6e175b822
hemaswaroopa12 <hemaswaroopa329@gmail.com>
2026-06-12 12:01:29 +0530

    js

---
+++ manipulation.js
@@ -0,0 +1,10 @@
+let pen=document.getElementById("wish");
+console.log(Pen);
+let mouse =document.getElementsByClassName("wishes");
+console.log(mouse);
+let cable=document.getElementsByClassName("wishes");
+console.log(cable);
+let water=document.getElementsByClassName("wishes");
+console.log(water);
+let queries=document.getElementsByClassName("wish");
+console.log(queries);
